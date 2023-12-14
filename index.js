9d8176ed7e9db7bec4fb2bdb248307545bf166b0
Allanshikomere <allanshikomere@gmail.com>
2023-12-14 23:32:58 +0300

    CompletedScopeLab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const leastFavoriteCustomer = 'Alice';
+var customerName;
+    customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+      }
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+    bestCustomer = newBestCustomer;
+  }
+function changeLeastFavoriteCustomer() {
+
+}
+
+function changeLeastFavoriteCustomer(newCustomer) {
+    leastFavoriteCustomer = newCustomer;
+  }
